b0211f8bf66de54fdafe0b676da871460336fdaa
Felipe Duarte <feduarte.dev@gmail.com>
2023-05-09 18:25:45 -0300

    setimo desafio completo

--- src/challenges.js
+++ src/challenges.js
@@ -75,6 +75,15 @@ const calcAllAreas = (base, height, form) => {
 
 // Desafio 7 - Crie a função catAndMouse
 
+const catAndMouse = (mouse, cat1, cat2) => {
+  if (Math.abs(mouse - cat1) < Math.abs(mouse - cat2)) {
+    return 'cat1';
+  } else if (Math.abs(mouse - cat1) > Math.abs(mouse - cat2)) {
+    return 'cat2';
+  }
+  return 'os gatos trombam e o rato foge';
+};
+
 // Desafio 8 - Crie a função fizzBuzz
 
 // Desafio 9 - Crie a função encode e a função decode
